34ccf28c7c5a3f4a4b5f670469ed6632d0703b3b
Vadim Sabadosh <vadimsabadosh@gmail.com>
2021-02-13 20:48:07 +0200

    Desk App (kanban) --  not finished

---
+++ build/precache-manifest.b64136a7274f521920d565fd5c88a57a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4334d138a671092ab6f35ac889f97309",
+    "url": "/index.html"
+  },
+  {
+    "revision": "55a174b449cc01b6381f",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "981288638087521acfc3",
+    "url": "/static/css/main.b956f539.chunk.css"
+  },
+  {
+    "revision": "55a174b449cc01b6381f",
+    "url": "/static/js/2.19707921.chunk.js"
+  },
+  {
+    "revision": "0d2572a88b364817964e936e89e4aa39",
+    "url": "/static/js/2.19707921.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "981288638087521acfc3",
+    "url": "/static/js/main.8d41a72e.chunk.js"
+  },
+  {
+    "revision": "a127bde4dc287848d1ba",
+    "url": "/static/js/runtime-main.2e1eb3c4.js"
+  }
+]);
